32096c2854c9df8ba70007c76d3da0b8c77b6cc8
laura <laura>
2025-01-30 10:56:32 +0100

    cambio true

--- NumeroOrdenado.java
+++ NumeroOrdenado.java
@@ -7,7 +7,7 @@ public class NumeroOrdenado {
   
         for (int i = 0; i < numbers.length - 1; i++) { 
             if (numbers[i] > numbers[i + 1]) {
-                return false;
+                return true;
             }
         }
         return true;
